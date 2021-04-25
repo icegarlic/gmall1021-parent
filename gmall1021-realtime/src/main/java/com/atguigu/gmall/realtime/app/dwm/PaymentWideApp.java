1e39d49b5a9681f456e6709a5f6ab4133cc3a75a
zhaohan <577530562@qq.com>
2021-04-25 23:54:13 +0800

    支付宽表app

--- gmall1021-realtime/src/main/java/com/atguigu/gmall/realtime/app/dwm/PaymentWideApp.java
+++ gmall1021-realtime/src/main/java/com/atguigu/gmall/realtime/app/dwm/PaymentWideApp.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.atguigu.gmall.realtime.bean.OrderWide;
 import com.atguigu.gmall.realtime.bean.PaymentInfo;
+import com.atguigu.gmall.realtime.bean.PaymentWide;
 import com.atguigu.gmall.realtime.utils.DateTimeUtil;
 import com.atguigu.gmall.realtime.utils.MyKafkaUtil;
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
@@ -13,14 +14,20 @@ import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.ProcessJoinFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+import java.time.Duration;
 
 /**
  * 支付宽表处理的应用
  */
 public class PaymentWideApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //TODO 1.基本环境准备
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         //1.1 设置并行度
@@ -50,24 +57,48 @@ public class PaymentWideApp {
 
         //TODO 4.设置Watermark以及提取事件时间字段
         //4.1 订单宽表(创建时间ts)
-        orderWideJsonObjStream.assignTimestampsAndWatermarks(
-                WatermarkStrategy.<OrderWide>forMonotonousTimestamps()
-                .withTimestampAssigner(new SerializableTimestampAssigner<OrderWide>() {
-                    @Override
-                    public long extractTimestamp(OrderWide orderWide, long recordTimestamp) {
-                        String create_time = orderWide.getCreate_time();
-                        return DateTimeUtil.toTs(create_time);
-                    }
-                })
+        SingleOutputStreamOperator<OrderWide> orderWideJsonObjWithEventStream = orderWideJsonObjStream.assignTimestampsAndWatermarks(
+                WatermarkStrategy.<OrderWide>forBoundedOutOfOrderness(Duration.ofSeconds(5))
+                        .withTimestampAssigner(new SerializableTimestampAssigner<OrderWide>() {
+                            @Override
+                            public long extractTimestamp(OrderWide orderWide, long recordTimestamp) {
+                                return DateTimeUtil.toTs(orderWide.getCreate_time());
+                            }
+                        })
         );
         //4.2 支付表(回调时间ts)
+        SingleOutputStreamOperator<PaymentInfo> paymentJsonObjWithEventStream = paymentJsonObjStream.assignTimestampsAndWatermarks(
+                WatermarkStrategy.<PaymentInfo>forBoundedOutOfOrderness(Duration.ofSeconds(5))
+                        .withTimestampAssigner(new SerializableTimestampAssigner<PaymentInfo>() {
+                            @Override
+                            public long extractTimestamp(PaymentInfo paymentInfo, long recordTimestamp) {
+                                return DateTimeUtil.toTs(paymentInfo.getCallback_time());
+                            }
+                        })
+        );
 
         //TODO 5.按照order_id对流中数据进行分组(指定双流join字段)
         //4.3 订单宽表(order_id)
+        KeyedStream<OrderWide, Long> orderWideKeyedStream = orderWideJsonObjWithEventStream.keyBy(OrderWide::getOrder_id);
         //4.3 支付表(order_id)
+        KeyedStream<PaymentInfo, Long> paymentInfoKeyedStream = paymentJsonObjWithEventStream.keyBy(PaymentInfo::getOrder_id);
 
         //TODO 6.双流join(intervalJoin)  a.intervalJoin(b).between.process
+        SingleOutputStreamOperator<PaymentWide> paymentWithOrderStream = orderWideKeyedStream
+                .intervalJoin(paymentInfoKeyedStream)
+                .between(Time.seconds(0), Time.seconds(1800))
+                .process(new ProcessJoinFunction<OrderWide, PaymentInfo, PaymentWide>() {
+                    @Override
+                    public void processElement(OrderWide orderWide, PaymentInfo paymentInfo, Context ctx, Collector<PaymentWide> out) throws Exception {
+                        out.collect(new PaymentWide(orderWide, paymentInfo));
+                    }
+
+                }).uid("payment_wide_join");
+        SingleOutputStreamOperator<String> paymentWideStream = paymentWithOrderStream.map(JSON::toJSONString);
+        paymentWideStream.print(">>>>");
 
         //TODO 7.将join之后的数据写回到kafka的dwm_payment_wide
+        paymentWideStream.addSink(MyKafkaUtil.getKafkaSink("dwm_payment_wide"));
+        env.execute();
     }
 }
