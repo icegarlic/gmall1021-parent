11848d21c9eb139b181ad57d250aee0f6f43bea2
zhaohan <577530562@qq.com>
2021-04-25 17:26:07 +0800

    更新

--- gmall1021-realtime/src/main/java/com/atguigu/gmall/realtime/utils/RedisUtil.java
+++ gmall1021-realtime/src/main/java/com/atguigu/gmall/realtime/utils/RedisUtil.java
@@ -7,8 +7,8 @@ import redis.clients.jedis.JedisPoolConfig;
 public class RedisUtil {
     private static JedisPool jedisPool;
 
-    public static Jedis getJedis(){
-        if(jedisPool == null){
+    public static Jedis getJedis() {
+        if (jedisPool == null) {
             JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
             jedisPoolConfig.setMinIdle(5);
             jedisPoolConfig.setMaxIdle(5);
@@ -16,9 +16,9 @@ public class RedisUtil {
             jedisPoolConfig.setBlockWhenExhausted(true);
             jedisPoolConfig.setTestOnBorrow(true);
             jedisPoolConfig.setMaxTotal(20);
-            jedisPool = new JedisPool(jedisPoolConfig,"hadoop102",6379,10000);
+            jedisPool = new JedisPool(jedisPoolConfig, "hadoop102", 6379, 10000);
+            System.out.println("开辟连接池");
         }
-        System.out.println("开辟连接池");
         return jedisPool.getResource();
     }
 
