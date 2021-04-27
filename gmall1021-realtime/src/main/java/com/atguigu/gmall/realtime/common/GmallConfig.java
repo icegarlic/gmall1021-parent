d151785d9fc55276f0c652327b9f593e794b24a2
zhaohan <577530562@qq.com>
2021-04-27 08:49:33 +0800

    更新

--- gmall1021-realtime/src/main/java/com/atguigu/gmall/realtime/common/GmallConfig.java
+++ gmall1021-realtime/src/main/java/com/atguigu/gmall/realtime/common/GmallConfig.java
@@ -4,6 +4,7 @@ package com.atguigu.gmall.realtime.common;
  * 项目配置常量类
  */
 public class GmallConfig {
-    public static final String HBASE_SCHEMA="GMALL1021_REALTIME";
+    public static final String HBASE_SCHEMA="GMALL2021_REALTIME";
     public static final String PHOENIX_SERVER="jdbc:phoenix:hadoop102,hadoop103,hadoop104:2181";
+    public static final String CLICKHOUSE_URL="jdbc:clickhouse://hadoop102:8123/default";
 }
