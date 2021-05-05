75bba148ad0d4f345fba15bc0cc520414d602b1c
zhaohan <577530562@qq.com>
2021-05-05 18:00:18 +0800

    将Product宽表写入kafka

--- gmall1021-realtime/src/main/java/com/atguigu/gmall/realtime/app/dws/ProductStatsApp.java
+++ gmall1021-realtime/src/main/java/com/atguigu/gmall/realtime/app/dws/ProductStatsApp.java
@@ -3,6 +3,7 @@ package com.atguigu.gmall.realtime.app.dws;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.atguigu.gmall.realtime.app.func.DimAsyncFunction;
 import com.atguigu.gmall.realtime.bean.OrderWide;
 import com.atguigu.gmall.realtime.bean.PaymentWide;
@@ -350,6 +351,11 @@ public class ProductStatsApp {
         productStatsWithTmStream.addSink(
                 ClickHouseUtil.<ProductStats>getJdbcSink(
                         "insert into product_stats_2021 values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"));
+
+        // TODO: 2021/5/5 将结果写入kafka
+        productStatsWithTmStream
+                .map(productStat -> JSON.toJSONString(productStat, new SerializeConfig(true)))
+                .addSink(MyKafkaUtil.getKafkaSink("dws_product_stats"));
 
         env.execute();
     }
